a3d3c017816ebee7273abb31b42e85de2ae8a375
agus_prayogi <agus21apy@gmail.com>
2021-02-25 09:54:07 +0700

    (+) add font awesome

--- index.php
+++ index.php
@@ -22,20 +22,21 @@ if (isset($_SESSION['login'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./assets/css/style.css">
+  <link rel="stylesheet" href="./assets/css/all.min.css">
   <title>Login SPP</title>
 </head>
 
 <body>
   <div class="wrapper">
     <div class="top">
-      <h1>Pembayaran SPP Online</h1>
+      <h1>Pembayaran SPP Online <i class="fas fa-camera"></i></h1>
       <h2 style="margin-bottom: 25px;">Masuk</h2>
       <form action="cek_login.php" class="form" method="POST" onsubmit="return validasi(this)">
         <div class="input_field">
-          <input type="text" id="username" name="inputusername" autocomplete="on" placeholder="USERNAME" required>
+          <input type="text" id="username" name="username" autocomplete="on" placeholder="USERNAME" required>
         </div>
         <div class="input_field">
-          <input type="Password" id="user-password" name="inputpassword" placeholder="PASSWORD" required>
+          <input type="Password" id="user-password" name="password" placeholder="PASSWORD" required>
         </div>
         <input class="btn" type="submit" name="submit" value="Masuk" />
       </form>
@@ -43,21 +44,23 @@ if (isset($_SESSION['login'])) {
   </div>
 
   <script type="text/javascript">
+    window.onload(e)
+
     function validasi(form) {
-      if (form.inputUsername.value == "") {
+      if (form.username.value == "") {
         alert("Anda belum mengisikan Username");
-        form.inputusername.focus();
+        form.username.focus();
         return false;
       }
-      if (form.inputPassword.value == "") {
+      if (form.password.value == "") {
         alert("Anda belum mengisikan Password.");
-        form.inputPassword.focus();
+        form.password.focus();
         return false;
       }
       return true;
     }
   </script>
-<script src="/js/script.js"></script>
+  <script src="./assets/js/script.js"></script>
 </body>
 
 </html>
